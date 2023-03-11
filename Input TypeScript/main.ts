2862e6d157128a4087ea2d5400d50bb6015b1f03
Yeongu <yeongu@student.ubc.ca>
2023-03-11 03:10:11 -0800

    1

--- Input TypeScript/main.ts
+++ Input TypeScript/main.ts
@@ -1,19 +1,45 @@
 import * as fs from "fs-extra";
 import * as parse5 from "parse5";
+import * as http from 'http';
 
 // Input: single html string (e.g DMP.htm)
 // Effect save room.json to following location
-export function parseRoomData(id: String, htmlData: String): void {
+export async function parseRoomData(id: String, htmlData: String): Promise<void> {
 
     let roomData = fs.readFileSync("DMP1.htm", "utf8")
 
     // console.log(roomData)
 
     const document1 = parse5.parse(roomData);
-    let fullBuildingName = findId(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value;
-    let roomNumbers = findId(document1, "class", "views-field views-field-field-room-furniture").value
+    // Getting the same things: full name, short name, address, lat, lon
+    let fullBuildingName = searchData(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value;
+    // let shortBuildingName
+    let address = searchData(document1, "id", "building-info").childNodes[3].childNodes[1].childNodes[0].value
 
+    let addressLongLat = await getLongLat(address)
+    let lattitude = addressLongLat.lat
+    let longitude = addressLongLat.lon
+
+    console.log(lattitude)
+    console.log(longitude)
+    let numberOfRooms = Math.floor(searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes.length / 2)
+
+    let i = 2
+    // getting different things: number, seats, type, furniture, href
+    let roomNumbers = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[1].childNodes[1].childNodes[0].value
+    let roomSeats = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[3].childNodes[0].value
+    let roomFurniture = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[5].childNodes[0].value
+    let roomType = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[7].childNodes[0].value
+    let roomHref = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[9].childNodes[1].attrs[0].value
+
+
+    // Print
+    console.log(numberOfRooms)
     console.log(roomNumbers)
+    console.log(roomSeats)
+    console.log(roomFurniture)
+    console.log(roomType)
+    console.log(roomHref)
 
     let roomJson = {
         [id + "_fullname"]: fullBuildingName,
@@ -28,24 +54,62 @@ export function parseRoomData(id: String, htmlData: String): void {
         [id + "_furniture"]: String,
         [id + "_href"]: String
     }
-
-
 }
 
 
 // recursively find id from html tree
-function findId(node: any, attributeType: String, attributeValue: String): any {
-    if (node.attrs && node.attrs.find((attr: any) => attr.name === attributeType && attr.value === attributeValue)) {
-        return node;
-    } else if (node.childNodes) {
-        for (const childNode of node.childNodes) {
-            const foundNode = findId(childNode, attributeType, attributeValue);
-            if (foundNode) {
-                return foundNode;
+// export function searchData(node: any, attributeType: String, attributeValue: String): any {
+//     if (node.attrs && node.attrs.find((res: any) => res.name === attributeType && res.value === attributeValue)) {
+//         return node;
+//     } else if (node.childNodes) {
+//         for (const childNode of node.childNodes) {
+//             const foundNode = searchData(childNode, attributeType, attributeValue);
+//             // if (foundNode != undefined) {
+//             //     return foundNode;
+//             // }
+//             (foundNode != undefined) ? (return foundNode) : null;
+//         }
+//     }
+//     return undefined;
+// }
+
+export function searchData(node: any, attributeType: String, attributeValue: String): any {
+    if (node.attrs) {
+        if (
+            node.attrs.find(
+                function (res: any) {
+                    return res.name === attributeType && res.value === attributeValue
+                }
+            )) {
+            return node;
+        }
+    }
+    if (node.childNodes) {
+        for (let childNode of node.childNodes) {
+            let result = searchData(childNode, attributeType, attributeValue);
+            if (result !== undefined) {
+                return result;
             }
         }
     }
     return undefined;
 }
+
+export function getLongLat(address: String): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+        let formattedAddress = "http://cs310.students.cs.ubc.ca:11316/api/v1/project_team088/" + address.replace(/ /g, "%20")
+        http.get(formattedAddress,
+            function (received: any) {
+                received.on('data', (requestedData: any) => {
+                    let JsonString = "" + requestedData;
+                    let longlatJson = JSON.parse(JsonString);
+                    resolve(longlatJson);
+                });
+            })
+
+    })
+}
+
 
 parseRoomData("DMP", "DMP.htm")
--- Output JavaScript/main.js
+++ Output JavaScript/main.js
@@ -22,43 +22,90 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseRoomData = void 0;
+exports.getLongLat = exports.searchData = exports.parseRoomData = void 0;
 const fs = __importStar(require("fs-extra"));
 const parse5 = __importStar(require("parse5"));
+const http = __importStar(require("http"));
 function parseRoomData(id, htmlData) {
-    let roomData = fs.readFileSync("DMP1.htm", "utf8");
-    const document1 = parse5.parse(roomData);
-    let fullBuildingName = findId(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value;
-    let roomNumbers = findId(document1, "class", "views-field views-field-field-room-furniture").value;
-    console.log(roomNumbers);
-    let roomJson = {
-        [id + "_fullname"]: fullBuildingName,
-        [id + "_shortname"]: String,
-        [id + "_number"]: String,
-        [id + "_name"]: String,
-        [id + "_address"]: String,
-        [id + "_lat"]: Number,
-        [id + "_lon"]: Number,
-        [id + "_seats"]: Number,
-        [id + "_type"]: String,
-        [id + "_furniture"]: String,
-        [id + "_href"]: String
-    };
+    return __awaiter(this, void 0, void 0, function* () {
+        let roomData = fs.readFileSync("DMP1.htm", "utf8");
+        const document1 = parse5.parse(roomData);
+        let fullBuildingName = searchData(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value;
+        let address = searchData(document1, "id", "building-info").childNodes[3].childNodes[1].childNodes[0].value;
+        let addressLongLat = yield getLongLat(address);
+        let lattitude = addressLongLat.lat;
+        let longitude = addressLongLat.lon;
+        console.log(lattitude);
+        console.log(longitude);
+        let numberOfRooms = Math.floor(searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes.length / 2);
+        let i = 2;
+        let roomNumbers = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[1].childNodes[1].childNodes[0].value;
+        let roomSeats = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[3].childNodes[0].value;
+        let roomFurniture = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[5].childNodes[0].value;
+        let roomType = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[7].childNodes[0].value;
+        let roomHref = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1].childNodes[9].childNodes[1].attrs[0].value;
+        console.log(numberOfRooms);
+        console.log(roomNumbers);
+        console.log(roomSeats);
+        console.log(roomFurniture);
+        console.log(roomType);
+        console.log(roomHref);
+        let roomJson = {
+            [id + "_fullname"]: fullBuildingName,
+            [id + "_shortname"]: String,
+            [id + "_number"]: String,
+            [id + "_name"]: String,
+            [id + "_address"]: String,
+            [id + "_lat"]: Number,
+            [id + "_lon"]: Number,
+            [id + "_seats"]: Number,
+            [id + "_type"]: String,
+            [id + "_furniture"]: String,
+            [id + "_href"]: String
+        };
+    });
 }
 exports.parseRoomData = parseRoomData;
-function findId(node, attributeType, attributeValue) {
-    if (node.attrs && node.attrs.find((attr) => attr.name === attributeType && attr.value === attributeValue)) {
-        return node;
+function searchData(node, attributeType, attributeValue) {
+    if (node.attrs) {
+        if (node.attrs.find(function (res) {
+            return res.name === attributeType && res.value === attributeValue;
+        })) {
+            return node;
+        }
     }
-    else if (node.childNodes) {
-        for (const childNode of node.childNodes) {
-            const foundNode = findId(childNode, attributeType, attributeValue);
-            if (foundNode) {
-                return foundNode;
+    if (node.childNodes) {
+        for (let childNode of node.childNodes) {
+            let result = searchData(childNode, attributeType, attributeValue);
+            if (result !== undefined) {
+                return result;
             }
         }
     }
     return undefined;
 }
+exports.searchData = searchData;
+function getLongLat(address) {
+    return new Promise((resolve, reject) => {
+        let formattedAddress = "http://cs310.students.cs.ubc.ca:11316/api/v1/project_team088/" + address.replace(/ /g, "%20");
+        http.get(formattedAddress, function (received) {
+            received.on('data', (requestedData) => {
+                let JsonString = "" + requestedData;
+                let longlatJson = JSON.parse(JsonString);
+                resolve(longlatJson);
+            });
+        });
+    });
+}
+exports.getLongLat = getLongLat;
 parseRoomData("DMP", "DMP.htm");
