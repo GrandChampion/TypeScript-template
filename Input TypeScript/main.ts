a5b2ab767a10feefd3b9529aa731b1b94bbdc33d
Yeongu <yeongu@student.ubc.ca>
2023-03-11 17:37:21 -0800

    1

--- Input TypeScript/main.ts
+++ Input TypeScript/main.ts
@@ -3,77 +3,83 @@ import * as parse5 from "parse5";
 import * as http from "http";
 
 
-
-
-
 export function stringList2jsonList(id: string, htmlArray: string[]) {
-    let jsonList = [];
+
+    const jsonList: any[] | PromiseLike<any[]> = []
+    let promises = []
+
     for (let html of htmlArray) {
-        let json = parseRoomData(id, html);
-        jsonList.push(json);
+        promises.push(parseRoomData(id, html))
     }
-    return jsonList;
+
+    Promise.all(promises).then((values) => {
+        for (let value of values) {
+            for (let json of value) {
+                jsonList.push(json)
+            }
+        }
+        return jsonList
+    })
 }
 
 // Input: single html string (e.g DMP.htm)
 // Effect save room.json to following location
-export async function parseRoomData(id: string, htmlData: string): Promise<void> {
+export async function parseRoomData(id: string, htmlData: string): Promise<{ [x: string]: any; }[]> {
     let roomData = fs.readFileSync("DMP1.htm", "utf8");
 
-    // console.log(roomData)
+    let result = []
 
     let document1 = parse5.parse(roomData);
     // Getting the same things: full name, short name, address, lat, lon
-    let fullBuildingName = searchData(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value.trim();
+    let fullBuildingName: string = searchData(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value.trim();
     // let shortBuildingName
-    let address = searchData(document1, "id", "building-info").childNodes[3].childNodes[0].childNodes[0].value.trim();
+    let roomAddress = searchData(document1, "id", "building-info").childNodes[3].childNodes[0].childNodes[0].value.trim();
+    let addressLongLat = await getLongLat(roomAddress);
+    let roomLatitude = addressLongLat.lat;
+    let roomLongitude = addressLongLat.lon;
 
-    let addressLongLat = await getLongLat(address);
-    let latitude = addressLongLat.lat;
-    let longitude = addressLongLat.lon;
-
-    // console.log(latitude);
-    // console.log(longitude);
     let numberOfRooms = Math.floor(
         searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes.length / 2
     );
 
-    let i = 3
-    // getting different things: number, seats, type, furniture, href
-    let roomNumbers = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-        .childNodes[1].childNodes[1].childNodes[0].value.trim();
-    let roomSeats = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-        .childNodes[3].childNodes[0].value.trim();
-    let roomFurniture = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-        .childNodes[5].childNodes[0].value.trim();
-    let roomType = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-        .childNodes[7].childNodes[0].value.trim();
-    let roomHref = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-        .childNodes[9].childNodes[1].attrs[0].value.trim();
+    for (let i = 1; i <= numberOfRooms; i++) {
+        // getting different things: number, seats, type, furniture, href
+        let roomNumber = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+            .childNodes[1].childNodes[1].childNodes[0].value.trim();
+        let roomSeats = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+            .childNodes[3].childNodes[0].value.trim();
+        let roomFurniture = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+            .childNodes[5].childNodes[0].value.trim();
+        let roomType = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+            .childNodes[7].childNodes[0].value.trim();
+        let roomHref = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+            .childNodes[9].childNodes[1].attrs[0].value.trim();
 
-    // Print
-    console.log(fullBuildingName)
-    console.log(address);
-    console.log(roomNumbers);
-    console.log(roomSeats);
-    console.log(roomFurniture);
-    console.log(roomType);
-    console.log(roomHref);
+        // Print
+        // console.log(fullBuildingName)
+        // console.log(roomAddress);
+        // console.log(roomNumber);
+        // console.log(roomSeats);
+        // console.log(roomFurniture);
+        // console.log(roomType);
+        // console.log(roomHref);
 
-
-    let roomJson = {
-        [id + "_fullname"]: fullBuildingName,
-        [id + "_shortname"]: String,
-        [id + "_number"]: String,
-        [id + "_name"]: String,
-        [id + "_address"]: String,
-        [id + "_lat"]: Number,
-        [id + "_lon"]: Number,
-        [id + "_seats"]: Number,
-        [id + "_type"]: String,
-        [id + "_furniture"]: String,
-        [id + "_href"]: String,
-    };
+        let roomJson = {
+            [id + "_fullname"]: fullBuildingName,
+            [id + "_shortname"]: getShortName(fullBuildingName),
+            [id + "_number"]: roomNumber,
+            [id + "_name"]: getShortName(fullBuildingName) + "_" + roomNumber,
+            [id + "_address"]: roomAddress,
+            [id + "_lat"]: roomLatitude,
+            [id + "_lon"]: roomLongitude,
+            [id + "_seats"]: roomSeats,
+            [id + "_type"]: roomType,
+            [id + "_furniture"]: roomFurniture,
+            [id + "_href"]: roomHref,
+        };
+        result.push(roomJson)
+    }
+    return result;
 }
 
 export function searchData(node: any, attributeType: string, attributeValue: string): any {
@@ -111,4 +117,89 @@ export function getLongLat(address: string): Promise<any> {
     });
 }
 
-parseRoomData("DMP", "DMP.htm");
+parseRoomData("DMP", "DMP.htm").then((a) => {
+    console.log(a)
+})
+
+
+
+export function getShortName(longBuildingName: string) {
+
+    let LongShortMap = new Map<string, string>([
+        ["Acute Care Unit", "ACU"],
+        ["Allard Hall (LAW)", "ALRD"],
+        ["Anthropology and Sociology", "ANSO"],
+        ["Aquatic Ecosystems Research Laboratory", "AERL"],
+        ["Asian Centre", "ACEN"],
+        ["Audain Art Centre", "AAC"],
+        ["Auditorium", "AUDI"],
+        ["Auditorium Annex", "AUDX"],
+        ["B.C. Binnings Studio", "BINN"],
+        ["Biological Sciences", "BIOL"],
+        ["Brock Hall Annex", "BRKX"],
+        ["Buchanan", "BUCH"],
+        ["Buchanan Tower", "BUTO"],
+        ["C. K. Choi Building for The Institute of Asian Res", "CHOI"],
+        ["Centre for Interactive Research on Sustainability", "CIRS"],
+        ["Chan Centre", "CHAN"],
+        ["Chemical and Biological Engineering Building", "CHBE"],
+        ["Chemistry", "CHEM"],
+        ["Civil and Mechanical Engineering", "CEME"],
+        ["D.H. Copp", "COPP"],
+        ["David Lam Management Research Centre", "DLAM"],
+        ["Detwiller Pavilion", "HSCC"],
+        ["Dorothy Somerset Studio", "DSOM"],
+        ["Douglas Kenny", "KENN"],
+        ["Earth and Ocean Sciences - Main", "EOSM"],
+        ["Earth Sciences Building", "ESB"],
+        ["Food, Nutrition and Health", "FNH"],
+        ["Forest Sciences Centre", "FSC"],
+        ["Frank Forward", "FORW"],
+        ["Fred Kaiser", "KAIS"],
+        ["Frederic Lasserre", "LASR"],
+        ["Frederic Wood Theatre", "FRWO"],
+        ["Friedman Building", "FRDM"],
+        ["Geography", "GEOG"],
+        ["George Cunningham", "CUNN"],
+        ["Hebb", "HEBB"],
+        ["Hennings", "HENN"],
+        ["Henry Angus", "ANGU"],
+        ["Horticulture Building", "GREN"],
+        ["Hugh Dempster Pavilion", "DMP"],
+        ["Institute for Computing (ICICS/CS)", "ICCS"],
+        ["Iona Building", "IONA"],
+        ["Irving K Barber Learning Centre", "IBLC"],
+        ["J.B. MacDonald", "MCDN"],
+        ["Jack Bell Building for the School of Social Work", "SOWK"],
+        ["Leonard S. Klinck (also known as CSCI)", "LSK"],
+        ["Life Sciences Centre", "LSC"],
+        ["MacLeod", "MCLD"],
+        ["MacMillan", "MCML"],
+        ["Mathematics", "MATH"],
+        ["Mathematics Annex", "MATX"],
+        ["Medical Sciences Block C", "MEDC"],
+        ["Michael Smith Laboratories", "MSB"],
+        ["Music", "MUSC"],
+        ["Neville Scarfe", "SCRF"],
+        ["Orchard Commons", "ORCH"],
+        ["Pharmaceutical Sciences Building", "PHRM"],
+        ["Ponderosa Annex E", "PONE"],
+        ["Ponderosa Commons: Oak House", "PCOH"],
+        ["Ponderosa Office Annex F", "PONF"],
+        ["Ponderosa Office Annex H", "PONH"],
+        ["Robert F. Osborne Centre", "OSBO"],
+        ["School of Population and Public Health", "SPPH"],
+        ["Sing Tao", "SOJ"],
+        ["Student Recreation Centre", "SRC"],
+        ["The Leon and Thea Koerner University Centre", "UCLL"],
+        ["Theatre-Film Production Building", "TFPB"],
+        ["Theatre-Film Production Building Annex", "TFPX"],
+        ["War Memorial Gymnasium", "MGYM"],
+        ["Wayne and William White Engineering Design Centre", "EDC"],
+        ["Wesbrook", "WESB"],
+        ["West Mall Annex", "WMAX"],
+        ["West Mall Swing Space", "SWNG"],
+        ["Woodward (Instructional Resources Centre-IRC)", "WOOD"],
+    ]);
+    return LongShortMap.get(longBuildingName);
+}
--- Output JavaScript/main.js
+++ Output JavaScript/main.js
@@ -32,51 +32,64 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getLongLat = exports.searchData = exports.parseRoomData = void 0;
+exports.getShortName = exports.getLongLat = exports.searchData = exports.parseRoomData = exports.stringList2jsonList = void 0;
 const fs = __importStar(require("fs-extra"));
 const parse5 = __importStar(require("parse5"));
 const http = __importStar(require("http"));
+function stringList2jsonList(id, htmlArray) {
+    const jsonList = [];
+    let promises = [];
+    for (let html of htmlArray) {
+        promises.push(parseRoomData(id, html));
+    }
+    Promise.all(promises).then((values) => {
+        for (let value of values) {
+            for (let json of value) {
+                jsonList.push(json);
+            }
+        }
+        return jsonList;
+    });
+}
+exports.stringList2jsonList = stringList2jsonList;
 function parseRoomData(id, htmlData) {
     return __awaiter(this, void 0, void 0, function* () {
         let roomData = fs.readFileSync("DMP1.htm", "utf8");
+        let result = [];
         let document1 = parse5.parse(roomData);
         let fullBuildingName = searchData(document1, "id", "building-info").childNodes[1].childNodes[0].childNodes[0].value.trim();
-        let address = searchData(document1, "id", "building-info").childNodes[3].childNodes[0].childNodes[0].value.trim();
-        let addressLongLat = yield getLongLat(address);
-        let latitude = addressLongLat.lat;
-        let longitude = addressLongLat.lon;
+        let roomAddress = searchData(document1, "id", "building-info").childNodes[3].childNodes[0].childNodes[0].value.trim();
+        let addressLongLat = yield getLongLat(roomAddress);
+        let roomLatitude = addressLongLat.lat;
+        let roomLongitude = addressLongLat.lon;
         let numberOfRooms = Math.floor(searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes.length / 2);
-        let i = 3;
-        let roomNumbers = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-            .childNodes[1].childNodes[1].childNodes[0].value.trim();
-        let roomSeats = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-            .childNodes[3].childNodes[0].value.trim();
-        let roomFurniture = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-            .childNodes[5].childNodes[0].value.trim();
-        let roomType = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-            .childNodes[7].childNodes[0].value.trim();
-        let roomHref = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
-            .childNodes[9].childNodes[1].attrs[0].value.trim();
-        console.log(fullBuildingName);
-        console.log(address);
-        console.log(roomNumbers);
-        console.log(roomSeats);
-        console.log(roomFurniture);
-        console.log(roomType);
-        console.log(roomHref);
-        let roomJson = {
-            [id + "_fullname"]: fullBuildingName,
-            [id + "_shortname"]: String,
-            [id + "_number"]: String,
-            [id + "_name"]: String,
-            [id + "_address"]: String,
-            [id + "_lat"]: Number,
-            [id + "_lon"]: Number,
-            [id + "_seats"]: Number,
-            [id + "_type"]: String,
-            [id + "_furniture"]: String,
-            [id + "_href"]: String,
-        };
+        for (let i = 1; i <= numberOfRooms; i++) {
+            let roomNumber = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+                .childNodes[1].childNodes[1].childNodes[0].value.trim();
+            let roomSeats = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+                .childNodes[3].childNodes[0].value.trim();
+            let roomFurniture = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+                .childNodes[5].childNodes[0].value.trim();
+            let roomType = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+                .childNodes[7].childNodes[0].value.trim();
+            let roomHref = searchData(document1, "class", "views-table cols-5 table").childNodes[3].childNodes[2 * i - 1]
+                .childNodes[9].childNodes[1].attrs[0].value.trim();
+            let roomJson = {
+                [id + "_fullname"]: fullBuildingName,
+                [id + "_shortname"]: getShortName(fullBuildingName),
+                [id + "_number"]: roomNumber,
+                [id + "_name"]: getShortName(fullBuildingName) + "_" + roomNumber,
+                [id + "_address"]: roomAddress,
+                [id + "_lat"]: roomLatitude,
+                [id + "_lon"]: roomLongitude,
+                [id + "_seats"]: roomSeats,
+                [id + "_type"]: roomType,
+                [id + "_furniture"]: roomFurniture,
+                [id + "_href"]: roomHref,
+            };
+            result.push(roomJson);
+        }
+        return result;
     });
 }
 exports.parseRoomData = parseRoomData;
@@ -112,4 +125,86 @@ function getLongLat(address) {
     });
 }
 exports.getLongLat = getLongLat;
-parseRoomData("DMP", "DMP.htm");
+parseRoomData("DMP", "DMP.htm").then((a) => {
+    console.log(a);
+});
+function getShortName(longBuildingName) {
+    let LongShortMap = new Map([
+        ["Acute Care Unit", "ACU"],
+        ["Allard Hall (LAW)", "ALRD"],
+        ["Anthropology and Sociology", "ANSO"],
+        ["Aquatic Ecosystems Research Laboratory", "AERL"],
+        ["Asian Centre", "ACEN"],
+        ["Audain Art Centre", "AAC"],
+        ["Auditorium", "AUDI"],
+        ["Auditorium Annex", "AUDX"],
+        ["B.C. Binnings Studio", "BINN"],
+        ["Biological Sciences", "BIOL"],
+        ["Brock Hall Annex", "BRKX"],
+        ["Buchanan", "BUCH"],
+        ["Buchanan Tower", "BUTO"],
+        ["C. K. Choi Building for The Institute of Asian Res", "CHOI"],
+        ["Centre for Interactive Research on Sustainability", "CIRS"],
+        ["Chan Centre", "CHAN"],
+        ["Chemical and Biological Engineering Building", "CHBE"],
+        ["Chemistry", "CHEM"],
+        ["Civil and Mechanical Engineering", "CEME"],
+        ["D.H. Copp", "COPP"],
+        ["David Lam Management Research Centre", "DLAM"],
+        ["Detwiller Pavilion", "HSCC"],
+        ["Dorothy Somerset Studio", "DSOM"],
+        ["Douglas Kenny", "KENN"],
+        ["Earth and Ocean Sciences - Main", "EOSM"],
+        ["Earth Sciences Building", "ESB"],
+        ["Food, Nutrition and Health", "FNH"],
+        ["Forest Sciences Centre", "FSC"],
+        ["Frank Forward", "FORW"],
+        ["Fred Kaiser", "KAIS"],
+        ["Frederic Lasserre", "LASR"],
+        ["Frederic Wood Theatre", "FRWO"],
+        ["Friedman Building", "FRDM"],
+        ["Geography", "GEOG"],
+        ["George Cunningham", "CUNN"],
+        ["Hebb", "HEBB"],
+        ["Hennings", "HENN"],
+        ["Henry Angus", "ANGU"],
+        ["Horticulture Building", "GREN"],
+        ["Hugh Dempster Pavilion", "DMP"],
+        ["Institute for Computing (ICICS/CS)", "ICCS"],
+        ["Iona Building", "IONA"],
+        ["Irving K Barber Learning Centre", "IBLC"],
+        ["J.B. MacDonald", "MCDN"],
+        ["Jack Bell Building for the School of Social Work", "SOWK"],
+        ["Leonard S. Klinck (also known as CSCI)", "LSK"],
+        ["Life Sciences Centre", "LSC"],
+        ["MacLeod", "MCLD"],
+        ["MacMillan", "MCML"],
+        ["Mathematics", "MATH"],
+        ["Mathematics Annex", "MATX"],
+        ["Medical Sciences Block C", "MEDC"],
+        ["Michael Smith Laboratories", "MSB"],
+        ["Music", "MUSC"],
+        ["Neville Scarfe", "SCRF"],
+        ["Orchard Commons", "ORCH"],
+        ["Pharmaceutical Sciences Building", "PHRM"],
+        ["Ponderosa Annex E", "PONE"],
+        ["Ponderosa Commons: Oak House", "PCOH"],
+        ["Ponderosa Office Annex F", "PONF"],
+        ["Ponderosa Office Annex H", "PONH"],
+        ["Robert F. Osborne Centre", "OSBO"],
+        ["School of Population and Public Health", "SPPH"],
+        ["Sing Tao", "SOJ"],
+        ["Student Recreation Centre", "SRC"],
+        ["The Leon and Thea Koerner University Centre", "UCLL"],
+        ["Theatre-Film Production Building", "TFPB"],
+        ["Theatre-Film Production Building Annex", "TFPX"],
+        ["War Memorial Gymnasium", "MGYM"],
+        ["Wayne and William White Engineering Design Centre", "EDC"],
+        ["Wesbrook", "WESB"],
+        ["West Mall Annex", "WMAX"],
+        ["West Mall Swing Space", "SWNG"],
+        ["Woodward (Instructional Resources Centre-IRC)", "WOOD"],
+    ]);
+    return LongShortMap.get(longBuildingName);
+}
+exports.getShortName = getShortName;
